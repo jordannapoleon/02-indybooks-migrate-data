6e2e735fe34119bdbbe1984a6bf780d2a0f1f1b2
jordannapoleon <jordannagcaoili@gmail.com>
2026-03-03 18:25:33 -0800

    Updated createbook vm and view

--- IndyBooks/ViewModels/CreateBookVM.cs
+++ IndyBooks/ViewModels/CreateBookVM.cs
@@ -1,5 +1,6 @@
 
 using System.ComponentModel.DataAnnotations;
+using IndyBooks.Models;
 
 namespace IndyBooks.ViewModels;
 public class CreateBookVM
@@ -7,6 +8,20 @@ public class CreateBookVM
     //TODO: Add Properties for all of the data shown in the CreateBook View (see Fig.3)
     // Be sure to add Data Annotations for Validation and Error Messages as shown
     public long BookId { get; set; }
+
+   [Required(ErrorMessage = "Book title please")]
+    [Display(Name = "Title")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "Just make it up")]
+    public string SKU { get; set; }
+
+    [Required(ErrorMessage = "The value is invalid")]
+    public decimal Price { get; set; }
+
+    [Required(ErrorMessage = "Author's Name please")]
+    [Display(Name = "AuthorName")]
+    public Writer Author { get; set; }
     
 
 }
